46937bf5feb9d78a17912ee0c94f4549af06b5b5
hcguimaraes <hcnguimaraes@hotmail.com>
2026-03-11 00:57:49 +0000

    exe: 10,20,30....

---
+++ Python/Exercises_loop/exercise7.py
@@ -0,0 +1,12 @@
+'''
+Exercício 7: Faça um algoritmo que gere a seguinte série: 10, 20, 30, 40, ..... 980, 990, 1000.
+
+'''
+vezes = 0
+
+while vezes < 1000:
+     
+     salto = 10
+     vezes = vezes + 10
+     print(vezes,end = " ")
+     vezes +10
