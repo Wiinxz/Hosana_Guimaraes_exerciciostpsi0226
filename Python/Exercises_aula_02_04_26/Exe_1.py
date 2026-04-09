f99564cbe765072a217f9ee2a90932b8fad42583
hcguimaraes <hcnguimaraes@hotmail.com>
2026-04-09 17:24:25 +0100

    Exe1 - Nomes Validos e Invalidos

---
+++ Python/Exercises_aula_02_04_26/Exe_1.py
@@ -0,0 +1,48 @@
+'''
+Cria um programa que peça ao utilizador para introduzir o seu nome completo. O programa deve validar se o nome contém apenas letras e espaços, a primeira letra do nome deve ser sempre maiúscula e a seguir ao espaço também, usando os códigos ASCII de cada caractere.
+Exemplo:
+Pedro Pereira 
+
+Se o nome for válido, o programa deve exibir:
+ "Nome válido!"
+Caso contrário, deve exibir:
+ "Nome inválido: contém caracteres não permitidos."
+
+No caso de o programa encontrar um caractere invalido deve parar a execução.
+
+Exemplos Inválidos:
+Miguel PriMo
+Luis AnseLmo
+Guilherme ramos
+
+'''
+def check_string(n):
+    espera_maiuscula = True
+
+    for letter in n:
+        codigo = ord(letter)
+
+        if espera_maiuscula:
+            if 65 <= codigo <= 90:        # é maiúscula → válido
+                espera_maiuscula = False
+            else:                          # devia ser maiúscula mas não é → inválido
+                print("Nome inválido: contém caracteres não permitidos.")
+                return False
+
+        elif codigo == 32:                 # é espaço → ativa flag
+            espera_maiuscula = True
+
+        elif 97 <= codigo <= 122:          # é minúscula → válido
+            pass
+
+        else:                              # nada do anterior → inválido
+            print("Nome inválido: contém caracteres não permitidos.")
+            return False
+
+    print(f"O Nome {n} é válido!")
+    return True
+
+
+nome = input("Introduz o teu nome completo: ")
+check_string(nome)
+
